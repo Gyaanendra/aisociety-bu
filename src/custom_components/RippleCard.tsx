c1864e96d931a1958faa8753f8a64d74080e6aef
Samaksh Tyagi <140980901+samakshty@users.noreply.github.com>
2025-01-21 00:55:42 +0530

    Update RippleCard.tsx

--- src/custom_components/RippleCard.tsx
+++ src/custom_components/RippleCard.tsx
@@ -8,7 +8,7 @@ export function RippleCard() {
       <div className="text-center">
         <Ripple />
         <h2 className="text-4xl font-bold tracking-tighter sm:text-4xl md:text-5xl mb-6 ">
-          More Events Coming Soon!
+          Stay Tuned for AIS Events in Zenevia!
         </h2>
         {/* <p className="px-2 text-lg md:text-xl max-w-2xl text-center">
           Be part of a community that is passionate about Artificial
